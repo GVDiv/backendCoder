63b9e4414b6d818f5281f4eea081f17d76372c0b
Gabriel Villamayor <gabitokun@outlook.com>
2024-03-06 02:21:23 -0300

    00_intro

---
+++ 00_intro/activity.js
@@ -0,0 +1,15 @@
+const nombre = "Gabriel";
+let edad = 32;
+const precio = 65;
+const nombreDeSerie = "Dragon Ball Z";
+const peliculas = ["Kimetsu", "Spiderman", "Capitan America"];
+
+console.log(nombre);
+console.log(edad);
+console.log(precio);
+console.log(nombreDeSerie);
+console.log(peliculas);
+
+edad = edad + 2;
+
+console.log(edad)
---
+++ 00_intro/index.js
@@ -0,0 +1,19 @@
+console.log("Hola mundo");
+
+/* string */
+const cadenaDeTexto = "esto es una cadena de texto";
+/* number */
+const numero = 78555.35161;
+/*boolean*/
+const verdadero = true;
+
+const datos = { cadenaDeTexto, numero, verdadero };
+const arreglo = [65000, "bitcoin", true];
+
+datos.fechaDeNacimiento = new Date('Marzo 5')
+
+console.log(verdadero);
+console.log(cadenaDeTexto);
+console.log(numero);
+console.log(datos);
+console.log(arreglo);
